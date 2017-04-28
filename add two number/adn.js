32850785e48a7923ccafaa85fbb847820be988d7
anthonyyepeng <416129562@qq.com>
2017-04-28 14:16:59 +0800

    add java and add some note

--- add two number/adn.js
+++ add two number/adn.js
@@ -14,13 +14,13 @@
 // 时间复杂度O(n^2)  空间复杂度 O(n)
 var addTwoNumbers =function(l1,l2){
     var sum = l1.val+l2.val;
-    var listP1= l1.next;
-    var listP2= l2.next;
-    var l3 = new ListNode(sum%10);
+    var listP1= l1.next;//声明一个指针 来遍历l1
+    var listP2= l2.next;//plist 遍历l2
+    var l3 = new ListNode(sum%10);// 个位保留
     var listP3 = l3;
     sum = Math.floor(sum/10);
     while(listP1||listP2||sum!==0){
-        sum = sum+(listP1?listP1.val:0)+(listP2?listP2.val:0);
+        sum = sum+(listP1?listP1.val:0)+(listP2?listP2.val:0);//算出来
         listP3.next = new listNode(sum%10);
         listP3 = node.next;
         listP1 = listP1?listP1.next:null;
