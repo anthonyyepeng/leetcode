c5054c27a02780c849e7b9553852fc217bcb831e
anthonyyepeng <416129562@qq.com>
2018-04-04 22:02:19 +0800

    修正了第20题，添加了一种新的写法，使得代码的可读性更强，更容易理解

--- Question020/Q20.js
+++ Question020/Q20.js
@@ -6,7 +6,6 @@ var isValid = function(s) {
     if (s.length %2 !=0) return false;
     else if (s.length ==0) return true;
     var stack = [];
-
     s = s.split('');
     for (let i =0;i<s.length;i++){
         if(s[i]=="["||s[i]=="{"||s[i]=="("){
@@ -22,7 +21,26 @@ var isValid = function(s) {
             stack.pop();
         }
         else return false;
+    }
+    return !stack.length;
+};
 
+
+//更好的写法  使得代码更加简洁好看一点
+var isValid = function(s) {
+    if (s.length %2 !=0) return false;
+    else if (s.length ==0) return true;
+    var stack = [];
+    s = s.split('');
+    for (let i =0;i<s.length;i++){
+        switch(s[i]){
+            case "[":stack.push("]");break;
+            case "{":stack.push("}");break;
+            case "(":stack.push(")");break;
+            default :
+                if(stack.pop()!==s[i]) return false;
+                break;
+        }
     }
     return !stack.length;
 };
